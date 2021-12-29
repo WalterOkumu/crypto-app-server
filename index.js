fa1c0b34456309bc6c8eb3b1eb74a521026108c7
WalterOkumu <okumu.oriaro@gmail.com>
2021-12-29 16:29:57 +0300

    updated coinId end point

--- index.js
+++ index.js
@@ -16,19 +16,35 @@ app.get('/', (req, res) => {
   res.send('Welcome to the Crypto Verse Backend API')
 })
 
+const headers = {
+    'x-access-token': process.env.API_KEY
+}
+
 app.get('/coins', async (req, res) => {
 
-  const url = process.env.BASE_URL + '/coins'
-
-  const headers = {
-    'x-access-token': process.env.API_KEY
-  }
+  const url = process.env.BASE_URL + '/coins?limit=100'
 
   const response = await axios.get(url, {headers}).then(response => response.data).catch(error => console.log(error))
 
   res.send(
-    response.data
+    response
   )
 })
+
+app.get(`/coins/:coinId`, async (req, res) => {
+
+  const coinId = req.params.coinId
+
+  const baseUrl = process.env.BASE_URL
+
+  const url = `${baseUrl}/coin/${coinId}`
+
+  const response = await axios.get(url, {headers}).then(response => response.data).catch(error => console.log(error))
+
+  res.send(
+    response
+  )
+})
+
 
 app.listen(port, () => console.log(`Server is running on port ${port}`))
